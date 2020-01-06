3b14708d16cd868a5fc8e50259eca5c2f6cf6c04
Kévin Francart <kevin.francart3@gmail.com>
2020-01-06 19:46:43 +0100

    init

--- assets/js/bridge/bridge/root/methods.js
+++ assets/js/bridge/bridge/root/methods.js
@@ -170,7 +170,6 @@ export function changeRouting(
                 type: 'danger',
                 message: trans('front.root.ajax.change_routing.error', {}, 'bridge-general', this.state.context.locale.catalogue),
             });
-
         })
         .finally(() => {
             // END
